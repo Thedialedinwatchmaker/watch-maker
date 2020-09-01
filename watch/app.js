4998a76004e031d17b96b13f16e88ad1d25e5b14
ecbob <chestercervantes13@hotmail.com>
2020-09-01 00:50:37 -0700

    - use sendgrid for contact form - remove latest updates sample text - merge master into 1-contact-email

--- watch/app.js
+++ watch/app.js
@@ -1,25 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
-// Middleware function example
-const logger = (req, res, next) => {
-    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
-    next();
-};
-app.use(logger);
+require('dotenv').config()
 
 // Middleware Body Parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Use routes
-// app.use('/api/members', require('./routes/contact'));
 app.use('/send', require('./routes/contact'));
-// app.post('/send', (req, res) => {
-//     console.log(req.body);
-// });
-
 
 // Set __dirname/public as static folder
 app.use(express.static(path.join(__dirname, 'public')));
--- watch/routes/contact.js
+++ watch/routes/contact.js
@@ -1,28 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const nodemailer = require('nodemailer');
-
-// // Api example
-// const members = [
-//     {
-//         id: 1,
-//         name: 'Chester'
-//     },
-//     {
-//         id: 2,
-//         name: 'Bob'
-//     }
-// ];
-// router.get('/', (req, res) => {
-//     res.json(members);
-// });
-// router.get('/:id', (req, res) => {
-//     res.json(members.filter(member => member.id === parseInt(req.params.id)));
-// });
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 router.post('/', (req, res) => {
-    console.log(req.body);
-
     const output = `
         <p>You have a new contact request</p>
         <h3>Contact details</h3>
@@ -32,40 +13,23 @@ router.post('/', (req, res) => {
         </ul>
         <h3>Message:</h3>
         <p>${req.body.message}</p>
-    `;
+    `
+    ;
+    const msg = {
+        to: "chestercervantes13@gmail.com",
+        from: "hans43@ethereal.email",
+        subject: 'Watchmaker - New Contact Request',
+        html: output
+    };
 
-    // Generate test SMTP service account from ethereal.email
-    // Only needed if you don't have a real mail account for testing
-    let testAccount = nodemailer.createTestAccount();
+    sgMail.send(msg).then(() => {
+        console.log('Message sent');
+        res.send({success: true, icon: './checkmark.png'});
+    }).catch((error) => {
+        console.log(error.response.body)
+    })
 
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: testAccount.user, // generated ethereal user
-            pass: testAccount.pass, // generated ethereal password
-        },
-    });
-
-    // send mail with defined transport object
-    let info = transporter.sendMail({
-        from: '"Fred Foo 👻" <foo@example.com>', // sender address
-        to: "chestersirvantes@gmail.com", // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: output, // html body
-    });
-
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-
-    res.json({message: 'Email sent'});
 });
+
 
 module.exports = router;
